1e7e14731d3968b84540b5038cb9281e074e70c1
sanjit <sanjit.thakuri@braintip.ai>
2023-09-04 05:13:59 +0545

    db prisma

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,6 +1,11 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Home() {
-  return <p className="text-3xl font-bold text-indigo-500">This is a protected route</p>;
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
+    </div>
+  );
 }
